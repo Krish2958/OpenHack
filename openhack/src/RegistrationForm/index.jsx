89434a45938bb581129ee34931e9aefb07cfad90
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-07 19:35:47 +0000

    [Feature] Test Commit for Google Form Added

---
+++ openhack/src/RegistrationForm/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './RegistrationForm.css'
+export const RegistrationForm = () => {
+  return (
+    <div className="registration-container">
+      <iframe
+        title="OpenHack 2024 Registration"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSflTsCkJIeaQ1bT3Q86bNBTYV5McsgTT9BOdKFEgr657w-j3w/viewform?embedded=true"
+        width="1920px"
+        height="1080px"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+

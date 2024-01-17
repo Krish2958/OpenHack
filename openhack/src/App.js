8e7f0532756a9a42278ca4b328c5b9bb7f58d528
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-17 11:22:56 +0000

    [FEATURE] ADDED FAQs

--- openhack/src/App.js
+++ openhack/src/App.js
@@ -3,11 +3,12 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Logo } from './assets';
+import { FAQ } from './pages/FAQ';
 
 const App = () => {
   return (
       <Container>
-        <div className='coming-soon'>
+        {/* <div className='coming-soon'>
           <video autoPlay loop muted className='coming-soon__brand-logo'>
             <source src={Logo.OpenHackWebM} type="video/webm" />
             <source src={Logo.OpenHackMp4} type="video/mp4" />
@@ -15,7 +16,8 @@ const App = () => {
             </video>
         <h1 className='coming-soon__year'>2024</h1>
         <h3 className='coming-soon__text'>Coming Soon</h3>
-        </div>
+        </div> */}
+        <FAQ />
       </Container>
   );
 };

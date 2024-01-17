c5437038c1ab7c41a853505687c18d42f6f48802
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-17 07:13:05 +0000

    [FEATURE] Added Icon Library & Social Handle

--- openhack/src/App.js
+++ openhack/src/App.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Logo } from './assets';
+import * as Icon from 'react-bootstrap-icons';
 
 const App = () => {
   return (
@@ -15,6 +16,15 @@ const App = () => {
             </video>
         <h1 className='coming-soon__year'>2024</h1>
         <h3 className='coming-soon__text'>Coming Soon</h3>
+        <span className='coming-soon__icon-container'>
+        <a href="mailto:info_openhack@mlvti.ac.in" target="_blank" rel="noopener noreferrer">
+        <Icon.EnvelopeOpen className='coming-soon__icon' color='#8CE8E5' />
+      </a>
+
+      <a href="https://www.instagram.com/openhack2024/" target="_blank" rel="noopener noreferrer">
+        <Icon.Instagram className='coming-soon__icon' color='#8CE8E5' />
+      </a>
+        </span>
         </div>
       </Container>
   );

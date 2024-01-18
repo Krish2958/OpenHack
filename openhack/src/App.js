d979457acbbd9ffeed9ed90bacd34748641b7ebe
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-18 06:37:13 +0000

    [FIX]

--- openhack/src/App.js
+++ openhack/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Logo } from './assets';
+import { Assets } from './assets';
 import { ContactUs } from './pages/ContactUs';
 
 const App = () => {
@@ -10,8 +10,8 @@ const App = () => {
       <Container>
         <div className='coming-soon'>
           <video autoPlay loop muted className='coming-soon__brand-logo'>
-            <source src={Logo.OpenHackWebM} type="video/webm" />
-            <source src={Logo.OpenHackMp4} type="video/mp4" />
+            <source src={Assets.OpenHackWebM} type="video/webm" />
+            <source src={Assets.OpenHackMp4} type="video/mp4" />
             Oops! The OpenHack 2024 logo video couldn't be loaded. Stay tuned for exciting updates!
             </video>
         <h1 className='coming-soon__year'>2024</h1>

8b8f78993af8b5be9fe146b57eace029cabfdc17
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-19 06:28:08 +0000

    Contact US Rendered

--- openhack/src/App.js
+++ openhack/src/App.js
@@ -8,7 +8,7 @@ import { ContactUs } from './pages/ContactUs';
 const App = () => {
   return (
       <Container>
-        <div className='coming-soon'>
+        {/* <div className='coming-soon'>
           <video autoPlay loop muted className='coming-soon__brand-logo'>
             <source src={Assets.OpenHackWebM} type="video/webm" />
             <source src={Assets.OpenHackMp4} type="video/mp4" />
@@ -16,7 +16,8 @@ const App = () => {
             </video>
         <h1 className='coming-soon__year'>2024</h1>
         <h3 className='coming-soon__text'>Coming Soon</h3>
-        </div>
+        </div> */}
+        <ContactUs />
       </Container>
   );
 };

0ded38f3b634013ee53d32051be9000b9ade09bc
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-20 11:37:22 +0000

    Testing Timeline

--- openhack/src/App.js
+++ openhack/src/App.js
@@ -4,31 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Logo } from './assets';
 import * as Icon from 'react-bootstrap-icons';
+import Timeline from './components/Timeline';
 
 const App = () => {
   return (
-      <Container>
-        <div className='coming-soon'>
-          <video autoPlay loop muted className='coming-soon__brand-logo'>
-            <source src={Logo.OpenHackWebM} type="video/webm" />
-            <source src={Logo.OpenHackMp4} type="video/mp4" />
-            Oops! The OpenHack 2024 logo video couldn't be loaded. Stay tuned for exciting updates!
-            </video>
-        <h1 className='coming-soon__year'>2024</h1>
-        <h3 className='coming-soon__text'>Coming Soon</h3>
-        <span className='coming-soon__icon-container'>
-          <a href="tel:+917568379160" >
-            <Icon.Telephone className='coming-soon__icon' color='#8CE8E5' />
-          </a>
-          <a href="mailto:info_openhack@mlvti.ac.in" target="_blank" rel="noopener noreferrer">
-            <Icon.EnvelopeOpen className='coming-soon__icon' color='#8CE8E5' />
-          </a>
-          <a href="https://www.instagram.com/openhack2024/" target="_blank" rel="noopener noreferrer">
-            <Icon.Instagram className='coming-soon__icon' color='#8CE8E5' />
-          </a>
-        </span>
-        </div>
-      </Container>
+      <Timeline />
   );
 };
 
---
+++ openhack/src/components/Timeline/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Timeline.css';
+
+const Timeline = () => {
+  return (
+    <div className="timeline">
+      <div className="container left-container">
+        <div className="text-box">
+          <h2>Pre-Registeration-Time</h2>
+          <small>9:00 AM 24/02/2024</small>
+          <p>kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab </p>
+          <span className="left-container-arrow"></span>
+        </div>
+      </div>
+      <div className="container right-container">
+        <div className="text-box">
+          <h2>Orientation</h2>
+          <small>9:30-11:30 AM 24/02/2024</small>
+          <p>kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab </p>
+          <span className="right-container-arrow"></span>
+        </div>
+      </div>
+      <div className="container left-container">
+        <div className="text-box">
+          <h2>Coding session #1</h2>
+          <small>11:30 - 2:00 24/02/2024</small>
+          <p>kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab </p>
+          <span className="left-container-arrow"></span>
+        </div>
+      </div>
+      <div className="container right-container">
+        <div className="text-box">
+          <h2>Registeration</h2>
+          <small>9:15 18/02/2024</small>
+          <p>kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab </p>
+          <span className="right-container-arrow"></span>
+        </div>
+      </div>
+      <div className="container left-container">
+        <div className="text-box">
+          <h2>Registeration</h2>
+          <small>9:15 18/02/2024</small>
+          <p>kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab, kuch bhi hota h iss duniya mai khaalo sab </p>
+          <span className="left-container-arrow"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;

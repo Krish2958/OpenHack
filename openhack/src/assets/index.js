4933cab8d5804168fd80cc56493fc7112b67d57a
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-24 10:15:10 +0530

    Update index.js

--- openhack/src/assets/index.js
+++ openhack/src/assets/index.js
@@ -1,6 +1,6 @@
 import OpenHackWebM from './OpenHack_WEBM.webm';
 import OpenHackMp4 from './OpenHack_MP4.mp4';
-export const Logo = {
+export const Assets = {
     OpenHackWebM: OpenHackWebM,
     OpenHackMp4: OpenHackMp4,
 }

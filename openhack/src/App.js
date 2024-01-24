7c07bd594b13e2eebfb548f1512a3bf33db9023c
Krish Maheshwari <105627846+Krish2958@users.noreply.github.com>
2024-01-24 04:42:40 +0000

    FEATURE Revealed Date

--- openhack/src/App.js
+++ openhack/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Logo } from './assets';
+import { Assets } from './assets';
 import * as Icon from 'react-bootstrap-icons';
 
 const App = () => {
@@ -10,12 +10,13 @@ const App = () => {
       <Container>
         <div className='coming-soon'>
           <video autoPlay loop muted className='coming-soon__brand-logo'>
-            <source src={Logo.OpenHackWebM} type="video/webm" />
-            <source src={Logo.OpenHackMp4} type="video/mp4" />
+            <source src={Assets.OpenHackWebM} type="video/webm" />
+            <source src={Assets.OpenHackMp4} type="video/mp4" />
             Oops! The OpenHack 2024 logo video couldn't be loaded. Stay tuned for exciting updates!
             </video>
-        <h1 className='coming-soon__year'>2024</h1>
-        <h3 className='coming-soon__text'>Coming Soon</h3>
+        <h1 className='coming-soon__year'>24-25 FEB, 2024</h1>
+        <div className='name-container'><h3 className='coming-soon__text'>Registrations Open Soon.</h3>
+        <h3 className='coming-soon__text-1'>Stay Tuned.</h3></div>
         <span className='coming-soon__icon-container'>
           <a href="tel:+917568379160" >
             <Icon.Telephone className='coming-soon__icon' color='#8CE8E5' />

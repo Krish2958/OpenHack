d08214a3c79bf0657e62b84313569662e3f4a7c4
krish2958 <krishm.km17@gmail.com>
2024-02-11 13:39:46 +0530

    [FIX] OPTIMIZED NAVBAR

--- openhack/src/components/CustomNavbar/index.jsx
+++ openhack/src/components/CustomNavbar/index.jsx
@@ -1,33 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./CustomNavbar.css";
 import { Assets } from "../../assets";
 
 export const CustomNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavCollapse = () => {
+    setExpanded(false);
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
-    <Navbar className="custom-navbar" bg="dark" variant="dark" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand><img alt='OpenHack' className="brand-logo-nav" src={Assets.OpenHack} /></Navbar.Brand>
+    <Navbar
+      className="custom-navbar"
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      expanded={expanded}
+    >
+      <LinkContainer to="/" onClick={handleNavCollapse}>
+        <Navbar.Brand>
+          <img alt='OpenHack' className="brand-logo-nav" src={Assets.OpenHack} />
+        </Navbar.Brand>
       </LinkContainer>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
+      <Navbar.Collapse
+        id="basic-navbar-nav"
+        onSelect={handleNavCollapse}
+      >
         <Nav className="mr-auto">
-          <LinkContainer to="/schedule">
+          <LinkContainer to="/schedule" onClick={handleNavCollapse}>
             <Nav.Link>Schedule</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/sponsors">
+          <LinkContainer to="/sponsors" onClick={handleNavCollapse}>
             <Nav.Link>Sponsors</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/rules">
+          <LinkContainer to="/rules" onClick={handleNavCollapse}>
             <Nav.Link>Rules</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/faq">
+          <LinkContainer to="/faq" onClick={handleNavCollapse}>
             <Nav.Link>FAQ</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/contact-us">
+          <LinkContainer to="/contact-us" onClick={handleNavCollapse}>
             <Nav.Link>Contact Us</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/registration">
+          <LinkContainer to="/registration" onClick={handleNavCollapse}>
             <Button className='nav-button' variant="primary">Registration</Button>
           </LinkContainer>
         </Nav>
